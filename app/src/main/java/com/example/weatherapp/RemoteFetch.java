f525bfeac2bf8c75e0ef3c9381be2b030647c4e3
Wojciech Charuza <wojtek.charuza@gmail.com>
2020-09-02 17:19:23 +0200

    add RemoteFetch class

---
+++ app/src/main/java/com/example/weatherapp/RemoteFetch.java
@@ -0,0 +1,46 @@
+package com.example.weatherapp;
+
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RemoteFetch {
+
+    private static final String OPEN_WEATHER_MAP_API = "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+
+    public static JSONObject getJSON(Context context, String city) {
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_MAP_API, city));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.addRequestProperty("x-api-key", context.getString(R.string.open_weather_maps_app_id));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer json = new StringBuffer(1024);
+            String tmp = "";
+            while ((tmp = reader.readLine()) != null) {
+                json.append(tmp).append("\n");
+            }
+            reader.close();
+            JSONObject weatherData = new JSONObject(json.toString());
+
+            if (weatherData.getInt("cod") != 200) { //check if request is succesful
+                return null;
+            }
+            return weatherData;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
