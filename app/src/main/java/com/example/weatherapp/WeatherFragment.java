bb0e2496549607fcc85def05b7c85e3b6f6771a3
Wojciech Charuza <wojtek.charuza@gmail.com>
2020-09-03 10:44:35 +0200

    implement setWeatherIcon method

--- app/src/main/java/com/example/weatherapp/WeatherFragment.java
+++ app/src/main/java/com/example/weatherapp/WeatherFragment.java
@@ -97,7 +97,33 @@ public class WeatherFragment extends Fragment {
         }
     }
 
-    private void setWeatherIcon(int id, long l, long l1) {
+    private void setWeatherIcon(int currentId, long sunrise, long sunset) {
+        int id = currentId / 100;
+        String icon = "";
+        if (currentId == 800) {
+            long currentTime = new Date().getTime();
+            if (currentTime >= sunrise && currentTime < sunset) {
+                icon = getActivity().getString(R.string.weather_sunny);
+            } else {
+                icon = getActivity().getString(R.string.weather_clear_night);
+            }
+        } else {
+            switch(id) {
+                case 2: icon = getActivity().getString(R.string.weather_thunder);
+                        break;
+                case 3: icon = getActivity().getString(R.string.weather_drizzle);
+                        break;
+                case 5: icon = getActivity().getString(R.string.weather_foggy);
+                        break;
+                case 6: icon = getActivity().getString(R.string.weather_cloudy);
+                        break;
+                case 7: icon = getActivity().getString(R.string.weather_snowy);
+                        break;
+                case 8: icon = getActivity().getString(R.string.weather_rainy);
+                        break;
+            }
+        }
+        weatherIcon.setText(icon);
     }
 
 
